8995cb86b911b25b62dc75ca52b14bfe89084fe0
Juliana Haurilava <jrediss.k@gmail.com>
2022-08-17 15:26:37 +0300

    fifth homework

---
+++ task_5.1/task_5.1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+  
+
+namespace task_5._1
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Создает из строки массив слов
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns>Массив из слов предложения</returns>
+        static string[] ToArray(string inputString)
+        {
+            return inputString.Split(' ');
+        }
+
+        /// <summary>
+        /// Выводит каждое слово предложения на отдельной строке
+        /// </summary>
+        /// <param name="arrayOfWords"></param>
+        static void OneWordOneLinePrint(string[] arrayOfWords)
+        {
+            foreach (string word in arrayOfWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputString = Console.ReadLine();
+            string[] arrayOfWords = ToArray(inputString);
+            OneWordOneLinePrint(arrayOfWords);
+        }
+    }
+}
---
+++ task_5.2/task_5.2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace task_5._1
+{
+    class Program
+    {
+        /// <summary>
+        /// Создает из строки массив слов
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns>Массив из слов предложения</returns>
+        static string[] ToArray(string str)
+        {
+            return str.Split(' ');
+        }
+
+        /// <summary>
+        /// Меняет порядок слов в предложении на обратный
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns>Предложение с обратным порядком слов</returns>
+        static string ReverseSentence(string inputString)
+        {
+            string[] arrayOfWords = ToArray(inputString);
+            Array.Reverse(arrayOfWords);
+            return String.Join(' ', arrayOfWords);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputString = Console.ReadLine();
+
+            Console.WriteLine(ReverseSentence(inputString));
+        }
+    }
+}
